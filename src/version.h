f66e9cc1e4567dc0027634cba3b3f47883544950
MaxXSoft <x@MaxXSoft.net>
2019-07-12 21:57:00 +0800

    (version) added header file 'version.h'

---
+++ src/version.h
@@ -0,0 +1,26 @@
+#ifndef ZODIA_VERSION_H_
+#define ZODIA_VERSION_H_
+
+// version information of Zodia
+
+#ifndef APP_NAME
+#define APP_NAME            "Zodia"
+#endif
+
+#ifndef APP_VERSION
+#define APP_VERSION         "0.0.0"
+#endif
+
+#ifndef APP_VERSION_MAJOR
+#define APP_VERSION_MAJOR   0
+#endif
+
+#ifndef APP_VERSION_MINOR
+#define APP_VERSION_MINOR   0
+#endif
+
+#ifndef APP_VERSION_PATCH
+#define APP_VERSION_PATCH   0
+#endif
+
+#endif  // ZODIA_VERSION_H_
