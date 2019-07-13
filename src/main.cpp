7f6ce04727f418ffe5335aa6e63c041638cfa506
MaxXSoft <x@MaxXSoft.net>
2019-07-13 20:38:15 +0800

    (main) completed file 'main.cpp'

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,55 @@
-//
+#include <iostream>
+#include <string>
+
+#include "ionia/src/util/argparse.h"
+#include "version.h"
+#include "game/gamewin.h"
+
+using namespace std;
+using namespace ionia::util;
+
+namespace {
+
+// display version info
+void PrintVersion() {
+  cout << APP_NAME << " version " << APP_VERSION << endl;
+  cout << "A toy 2D game engine, based on SDL2 and Ionia." << endl;
+  cout << endl;
+  cout << "Copyright (C) 2010-2019 MaxXing, MaxXSoft. License GPLv3.";
+  cout << endl;
+}
+
+}
 
 int main(int argc, const char *argv[]) {
-  //
+  // initialize argument parser
+  ArgParser argp;
+  argp.AddArgument<string>("input", "input Zodia game map file");
+  argp.AddOption<bool>("help", "h", "show this message", false);
+  argp.AddOption<bool>("version", "v", "show version info", false);
+  // parse arguments
+  if (!argp.Parse(argc, argv)) {
+    cerr << "invalid input, run '";
+    cerr << argp.program_name() << " -h' for help" << endl;
+    return 1;
+  }
+  else if (argp.GetValue<bool>("help")) {
+    argp.PrintHelp();
+  }
+  else if (argp.GetValue<bool>("version")) {
+    PrintVersion();
+  }
+  else {
+    // run game
+    try {
+      GameWindow game;
+      game.LoadMap(argp.GetValue<string>("input"));
+      game.Run();
+    }
+    catch (const std::runtime_error &e) {
+      cerr << "Zodia terminated unexpectedly due to an error: ";
+      cerr << e.what() << endl;
+    }
+  }
   return 0;
 }
