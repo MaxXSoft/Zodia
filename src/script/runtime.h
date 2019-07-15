03777819fa59d322ce154a2d716d786b7ffecabc
MaxXSoft <x@MaxXSoft.net>
2019-07-15 18:38:36 +0800

    (runtime) modified some details

--- src/script/runtime.h
+++ src/script/runtime.h
@@ -6,17 +6,30 @@
 
 #include "ionia/src/vm/vm.h"
 
-// interface of runtime library in script host
-class RuntimeInterface {
+// forward declaration of script host
+class ScriptHost;
+
+// base class of runtime library in script host
+class RuntimeBase {
  public:
+  RuntimeBase(const ScriptHost &host) : host_(host) {}
+
   // handler of library
   virtual bool Handler(const std::string &path,
                        ionia::vm::VM::ValueStack &vals,
                        ionia::vm::Value &ret) = 0;
+
+  // getters
   // get library name
   virtual const std::string &name() const = 0;
+
+ protected:
+  const ScriptHost &host() const { return host_; }
+
+ private:
+  const ScriptHost &host_;
 };
 
-using RuntimePtr = std::unique_ptr<RuntimeInterface>;
+using RuntimePtr = std::unique_ptr<RuntimeBase>;
 
 #endif  // ZODIA_SCRIPT_RUNTIME_H_
