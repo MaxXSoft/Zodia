e390d525f2bb18c183b8fe0ecf3f8735fce2755e
MaxXSoft <x@MaxXSoft.net>
2019-07-15 16:40:21 +0800

    (generalscene) modified some details

--- src/game/generalscene.h
+++ src/game/generalscene.h
@@ -15,27 +15,23 @@ class GeneralScene : public GameSceneInterface {
 
   // event handlers
   void Reset() override {
-    if (!reset_handler_.empty()) host_.CallFunction(reset_handler_);
+    if (!reset_.empty()) host_.CallResetHandler(reset_);
   }
 
-  void FrameBegin(KeyStatus status) override {
-    if (!begin_handler_.empty()) host_.CallFunction(begin_handler_);
+  void FrameBegin(KeyStatus key) override {
+    if (!begin_.empty()) host_.CallBeginHandler(begin_, key);
   }
 
   const ScenePtr &GetScene() const override { return scene_; }
 
   // setters
-  void set_reset_handler(const std::string &name) {
-    reset_handler_ = name;
-  }
-  void set_begin_handler(const std::string &name) {
-    begin_handler_ = name;
-  }
+  void set_reset_handler(const std::string &name) { reset_ = name; }
+  void set_begin_handler(const std::string &name) { begin_ = name; }
 
  private:
   ScriptHost &host_;
   ScenePtr scene_;
-  std::string reset_handler_, begin_handler_;
+  std::string reset_, begin_;
 };
 
 #endif  // ZODIA_GAME_GENERALSCENE_H_
